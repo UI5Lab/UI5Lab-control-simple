ed4f869b8a6a5382a341c3685330fa5de724190f
Michael Graf <mi.graf@sap.com>
2017-04-26 21:20:42 +0200

    added control implementation

---
+++ src/ui5lab/control/Square.js
@@ -0,0 +1,62 @@
+sap.ui.define([
+	'jquery.sap.global',
+	'sap/ui/core/Control'
+],
+function(jQuery, Control) {
+	"use strict";
+
+	/**
+	 * Constructor for a new Square
+	 */
+	var oSquare = Control.extend("ui5lab.control.Square", {
+		/**
+		 * Control API
+		 */
+		metadata: {
+			properties: {
+				/**
+				 * Sets the size (width/height) for the square
+				 */
+				size: {type: "int", defaultValue: "50"},
+				/**
+				 * Sets the text inside the square
+				 */
+				text: {type: "string", defaultValue: ""}
+			}
+		},
+
+		/**
+		 * Lifecycle hook to initialize the control
+		 */
+		init: function () {
+			// nothing yet
+		},
+
+		/**
+		 * Renders the HTML for the control, using the provided {@link sap.ui.core.RenderManager}.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm RenderManager object
+		 * @param {sap.ui.core.Control} oControl An object representation of the control that will be rendered
+		 */
+		renderer: function (oRm, oControl) {
+			oRm.write("<div");
+			oRm.writeControlData(oControl);
+
+			oRm.addClass("ui5labSquare");
+			oRm.writeClasses();
+
+			oRm.addStyle("width", oControl.getSize() + "px");
+			oRm.addStyle("height", oControl.getSize() + "px");
+			oRm.writeStyles();
+
+			oRm.write(">");
+
+			oRm.writeEscaped(oControl.getText());
+
+			oRm.write("</div>");
+		}
+	});
+
+	return oSquare;
+			
+});
